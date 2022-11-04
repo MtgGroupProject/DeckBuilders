ad24e468c21a2451fb1d635e0a775a3afa1b6cfa
mmm <margaret.m.matson@gmail.com>
2022-11-03 21:39:59 -0500

    added fetch to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+fetch('https://api.scryfall.com/cards/named?fuzzy=aust+com',{ 
 
+
+  method: 'GET', //GET is the default.
+  credentials: 'same-origin', // include, *same-origin, omit
+  redirect: 'follow', // manual, *follow, error
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
